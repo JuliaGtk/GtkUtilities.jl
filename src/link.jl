741d594697bfc01e818ee96f87afcbea01890148
Tim Holy <tim.holy@gmail.com>
2015-08-28 07:43:48 -0500

    Add support for Label, improve show methods

--- src/link.jl
+++ src/link.jl
@@ -53,6 +53,17 @@ function set!{T}(state::State{T}, value)
     state
 end
 
+# Set the value, skipping the input widget
+function set!{T}(state::State{T}, widget::AbstractLinkedWidget)
+    value = get(widget)
+    state.value = value
+    for w in state.widgets
+        w == widget && continue
+        set_quietly!(w, value)
+    end
+    state
+end
+
 ### Widgets linked to AbstractState objects
 
 type LinkedWidget{T,W} <: AbstractLinkedWidget{T,W}
@@ -60,26 +71,36 @@ type LinkedWidget{T,W} <: AbstractLinkedWidget{T,W}
     id::UInt
 end
 
+# For types like Label that don't have signals usable for set!
+type LinkedWidgetStored{T,W,S<:AbstractState} <: AbstractLinkedWidget{T,W}
+    widget::W
+    id::UInt
+    state::S
+end
+
 @doc """
 `set(w, val)` sets the value of the linked widget `w` and fires
 the callback, thereby updating all other linked widgets.
 """ ->
-function set!(w::LinkedWidget, val)
+function set!(w::AbstractLinkedWidget, val)
     _set!(w, val)  # this might fire the callback, depending on the widget
     emit(w)        # provide this method for those that need explicit firing
     w
 end
 
-emit(w::LinkedWidget) = nothing   # fallback method
+set!(w::LinkedWidgetStored, val) = set!(w.state, val)
+
+emit(w::AbstractLinkedWidget) = nothing   # fallback method
 
 @doc """
 `set_quietly!(w, val)` sets the value of the linked widget `w` without
 firing the callback.
 """ ->
-function set_quietly!(w::LinkedWidget, val)
-    signal_handler_block(w.widget, w.id)
+function set_quietly!(w::AbstractLinkedWidget, val)
+    id = w.id
+    id != 0 && signal_handler_block(w.widget, w.id)
     _set!(w, val)
-    signal_handler_unblock(w.widget, w.id)
+    id != 0 && signal_handler_unblock(w.widget, w.id)
     w
 end
 
@@ -91,8 +112,12 @@ will henceforth be synchronized: calling `get(state)` or
 `set!(wlinked, val)` will change the value for all mutually-linked
 objects.
 """
-function link{T}(val::AbstractState{T}, w::Gtk.GtkWidget)
-    w = LinkedWidget{T,typeof(w)}(w, 0)
+function link{T}(val::AbstractState{T}, widget::Gtk.GtkWidget)
+    w = LinkedWidget{T,typeof(widget)}(widget, 0)
+    _link(val, w)
+end
+
+function _link{T}(val::AbstractState{T}, w::AbstractLinkedWidget)
     _set!(w, get(val))
     id = link(val, w)
     w.id = id
@@ -100,33 +125,39 @@ function link{T}(val::AbstractState{T}, w::Gtk.GtkWidget)
     w
 end
 
-function Base.show(io::IO, w::LinkedWidget)
-    print(io, "Linked ", typeof(w.widget).name.name, "(", get(w), ")")
+function Base.show(io::IO, w::AbstractLinkedWidget)
+    print(io, typeof(w).name.name, " ", typeof(w.widget).name.name, "(", get(w), ")")
     n = getproperty(w.widget, :name, ByteString)
     if !isempty(n)
         print(io, ",\"", n, "\"")
     end
 end
 
-## Scale
 
-function link{T,W<:Scale}(val::AbstractState{T}, w::LinkedWidget{T,W})
-    signal_connect(w.widget, :value_changed) do widget
-        set!(val, get(w))
-    end
+## Label
+
+function link{T}(val::AbstractState{T}, w::Label)
+    w = LinkedWidgetStored{T,typeof(w),typeof(val)}(w, 0, val)
+    _link(val, w)
 end
 
-function Base.get{T,W<:Scale}(w::LinkedWidget{T,W})
-    adj = @Adjustment(w.widget)
-    val = getproperty(adj, :value, Int)
+function link{T,W<:Label}(val::AbstractState{T}, w::LinkedWidgetStored{T,W})
+    0
+end
+
+function Base.get{T<:AbstractString,W<:Label}(w::LinkedWidgetStored{T,W})
+    val = getproperty(w.widget, :label, ByteString)
     convert(T, val)::T
 end
 
-function _set!{T,W<:Scale}(w::LinkedWidget{T,W}, value)
-    adj = @Adjustment(w.widget)
-    setproperty!(adj, :value, value)
+function Base.get{T<:Number,W<:Label}(w::LinkedWidgetStored{T,W})
+    val = getproperty(w.widget, :label, ByteString)
+    parse(T, val)::T
 end
 
+function _set!{T,W<:Label}(w::LinkedWidgetStored{T,W}, value)
+    setproperty!(w.widget, :label, string(value))
+end
 
 ## Entry
 
@@ -151,5 +182,25 @@ function _set!{T,W<:Entry}(w::LinkedWidget{T,W}, value)
 end
 
 emit{T,W<:Entry}(w::LinkedWidget{T,W}) = signal_emit(w.widget, :activate, Void)
+
+## Scale
+
+function link{T,W<:Scale}(val::AbstractState{T}, w::LinkedWidget{T,W})
+    signal_connect(w.widget, "value-changed") do widget
+#        @schedule set!(val, w)    # Gtk.jl #161
+        set!(val, w)
+    end
+end
+
+function Base.get{T,W<:Scale}(w::LinkedWidget{T,W})
+    adj = @Adjustment(w.widget)
+    val = getproperty(adj, :value, Int)
+    convert(T, val)::T
+end
+
+function _set!{T,W<:Scale}(w::LinkedWidget{T,W}, value)
+    adj = @Adjustment(w.widget)
+    setproperty!(adj, :value, value)
+end
 
 end
--- test/link.jl
+++ test/link.jl
@@ -5,7 +5,7 @@ e = @Entry()
 setproperty!(e, :name, "entry")
 sc = @Scale(false, 1:10)
 setproperty!(sc, :name, "scale")
-hbox = @Box(:h)
+hbox = @Box(:v)
 win = @Window(hbox, "Hi there!")
 push!(hbox, e)
 push!(hbox, sc)
@@ -32,3 +32,10 @@ set!(sclink, 4)
 @test get(state)  == 4
 @test get(elink)  == 4
 @test get(sclink) == 4
+
+l = @Label("hello")
+s = State("world")
+llink = link(s, l)
+@test get(llink) == "world"
+set!(llink, "Gtk")
+@test get(s) == "Gtk"
