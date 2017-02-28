7b207212345bafd03b394a2f28af748370827aef
Tim Holy <tim.holy@gmail.com>
2017-02-28 17:19:36 -0600

    Interpret numeric arrays as grayscale

--- test/utils.jl
+++ test/utils.jl
@@ -1,17 +1,36 @@
 using Gtk.ShortNames, Colors, FixedPointNumbers
 
-let c = @Canvas(), win = @Window(c, "Canvas1")
+let c = Canvas(), win = Window(c, "Canvas1")
     Gtk.draw(c) do widget
         fill!(widget, RGB(1,0,0))
     end
     showall(win)
 end
 
-let c = @Canvas(), win = @Window(c, "Canvas2")
+let c = Canvas(), win = Window(c, "Canvas2")
     Gtk.draw(c) do widget
         w, h = Int(width(widget)), Int(height(widget))
         randcol = reinterpret(RGB{N0f8}, rand(0x00:0xff, 3, w, h), (w, h))
         copy!(widget, randcol)
+    end
+    showall(win)
+end
+
+let c = Canvas(), win = Window(c, "Canvas3")
+    Gtk.draw(c) do widget
+        w, h = Int(width(widget)), Int(height(widget))
+        randnum = reinterpret(N0f8, rand(0x00:0xff, w, h))
+        copy!(widget, randnum)
+    end
+    showall(win)
+end
+
+let c = Canvas(), win = Window(c, "Canvas4")
+    Gtk.draw(c) do widget
+        w, h = Int(width(widget)), Int(height(widget))
+        img = zeros(w, h)
+        img[1,1] = 1.5
+        @test_throws ArgumentError copy!(widget, img)
     end
     showall(win)
 end
